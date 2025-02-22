8e6cba3fd507d6f94284cd44164cf2d7b6cdd950
joklar93 <113863324+joklar93@users.noreply.github.com>
2025-02-22 04:46:01 +0000

    .

---
+++ Mod2/binary_search.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int BinarySearch(double* numbers, int numbersSize, double key) {
+   // Variables to hold the low and high indices of the area being searched. 
+   // Starts with entire range.
+   int low = 0;
+   int high = numbersSize - 1;
+   
+   // Loop until "low" passes "high"
+   while (high >= low) {
+      // Calculate the middle index
+      int mid = (high + low) / 2;
+
+      // Cut the range to either the left or right half,
+      // unless numbers[mid] is the key
+      if (numbers[mid] < key) {
+         low = mid + 1;
+      }
+      else if (numbers[mid] > key) {
+         high = mid - 1;
+      }
+      else {
+         return mid;
+      }
+   }
+   
+   return -1; // not found
+}
+
+int main() {
+   double numbers[] = { 3.1415926535, 4.22222, 7.98765, 10.3231, 11.3, 32.66, 45.2, 87.3 };
+   int numbersSize = sizeof(numbers) / sizeof(numbers[0]);
+   cout << "NUMBERS: [" << numbers[0];
+   for (int i = 1; i < numbersSize; i++) {
+      cout << ", " << numbers[i];
+   }
+   cout << "]" << endl;
+      
+   // Prompt for an integer to search for
+   cout << "Enter a double value: ";
+   double key = 0;
+   cin >> key;
+      
+   int keyIndex = BinarySearch(numbers, numbersSize, key);
+   if (keyIndex == -1) {
+      cout << key << " was not found." << endl;
+   }
+   else {
+      cout << "Found " << key << " at index ";
+      cout << keyIndex << "." << endl;
+   }
+}
